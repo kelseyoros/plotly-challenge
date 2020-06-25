467244d1bff3f27be4acff7351b89e09845553bd
Oros <koros@nn.local>
2020-06-25 18:07:40 -0500

    adding finished js file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,35 +7,58 @@ function optionChanged(sampleName) {
 };
 
 function init() {
-      // Use D3 to select the dropdown menu
-  var selector = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  d3.json("static/js/data/samples.json").then((data) => {
+    // Use D3 to select the dropdown menu
+    var dropdown = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    d3.json("static/js/data/samples.json").then((data) => {
 
-    var samples = data.names;
-    samples.forEach(sample => selector.append('option').text(sample).property('value', sample))
+        var samples = data.names;
+        samples.forEach(sample => dropdown.append('option').text(sample).property('value', sample))
     
-    var firstSample = samples[0];
-    buildCharts(firstSample);
-    buildMetaData(firstSample);
+        var firstSample = samples[0];
+        buildCharts(firstSample);
+        buildMetaData(firstSample);
+    });
+};
+
+function buildMetaData(sample) {
+    //Use d3 to read in the json file
+    d3.json("static/js/data/samples.json").then((data) => {
+        
+        var metadata = data.metadata.filter(sampleDict => sampleDict.id.toString() === sample)[0];
+
+        //Use d3 to select the metadata id
+        var demographicInfo = d3.select("#sample-metadata");
+
+        demographicInfo.html("");
+
+        Object.entries(metadata).forEach((key) => {
+            demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n")
+        });
     });
 };
 
 function buildCharts (sampleName) {
     d3.json("static/js/data/samples.json").then((data) => {
 
-        var samples = data.samples
-        var sampleData = samples.filter(sampleDict => sampleDict.id == sampleName)
-        var sampleData = sampleData[0]
+        var samples = data.samples.filter(sampleDict => sampleDict.id === sampleName)[0];
+
+        var samplevalues = samples.sample_values.slice(0,10).reverse();
+
+        var OTU_top = samples.otu_ids.slice(0,10).reverse();
+
+        var labels = samples.otu_labels.slice(0,10).reverse();
+
+        var OTU_id = OTU_top.map(sampleNum => "OTU " + sampleNum)
 
 
     //  Create the trace for the bar chart
     var barTrace = {
-      x: sampleData.sample_values.slice(0,10).reverse(),
-      y: sampleData.otu_ids.slice(0,10).reverse(),
+      x: samplevalues,
+      y: OTU_id,
       type: "bar",
       orientation: "h",
-      text: sampleData.otu_labels.slice(0,10).reverse()
+      text: labels
     };
 
 
@@ -45,9 +68,7 @@ function buildCharts (sampleName) {
   
     // Define the plot layout
     var barLayout = {
-      title: "Top 10 OTUs",
-//     xaxis: { title: "Organ" },
-//     yaxis: { title: "Square Root of Survival" }
+      title: "Top 10 OTU",
     };
   
     // Plot the chart to a div tag with id "bar"
@@ -55,15 +76,15 @@ function buildCharts (sampleName) {
 
     // Create the trace for the bubble chart
     var bubbleTrace = {
-        x: data.otu_ids,
-        y: data.sample_values,
+        x: samples.otu_ids,
+        y: samples.sample_values,
+        text: samples.otu_labels,
         mode: 'markers',
-        text: data.otu_labels,
         marker: {
-            size: data.sample_values,
-            color: data.otu_ids
+            size: samples.sample_values,
+            color: samples.otu_ids
           }        
-        }
+        };
 
     // Create the data array for the plot
     var bubbleData = [bubbleTrace];
@@ -71,8 +92,6 @@ function buildCharts (sampleName) {
     //Define the plot layout
     var bubbleLayout = {
         xaxis: {title: 'OTU ID'},
-//        height: ,
-//        width: ,
     };
 
     Plotly.newPlot("bubble", bubbleData, bubbleLayout)
